debf96c00ceda49be965259e1823ebed0e986431
EmelKrist <kristina_1516@mail.ru>
2024-06-03 14:46:42 +0800

    Change the database of the quiz questions

--- script.js
+++ script.js
@@ -1,41 +1,4 @@
-const questions = [
-  {
-    question: "Which is the largest animal in the world?",
-    answers: [
-      { text: "Shark", correct: false },
-      { text: "Blue whale", correct: true },
-      { text: "Elephant", correct: false },
-      { text: "Giraffe", correct: false },
-    ],
-  },
-  {
-    question: "Which is the smallest country in the world?",
-    answers: [
-      { text: "Vatican City", correct: true },
-      { text: "Blue Bhutan", correct: false },
-      { text: "Nepal", correct: false },
-      { text: "Shri Lanka", correct: false },
-    ],
-  },
-  {
-    question: "Which is largest desert in the world?",
-    answers: [
-      { text: "Kalahari", correct: false },
-      { text: "Gobi", correct: false },
-      { text: "Sahara", correct: false },
-      { text: "Antarctica", correct: true },
-    ],
-  },
-  {
-    question: "Which is the smallest continent in the world?",
-    answers: [
-      { text: "Asia", correct: false },
-      { text: "Australia", correct: true },
-      { text: "Arctic", correct: false },
-      { text: "Africa", correct: false },
-    ],
-  },
-];
+import { questions } from "./questions.js";
 
 const questionElement = document.getElementById("question");
 const answerButtons = document.getElementById("answer-buttons");
